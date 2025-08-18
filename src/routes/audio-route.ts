6470465385ab848f3434fed31aa09f897a8772b5
webster-rafael <webster.rafael2014@gmail.com>
2025-08-17 20:32:33 -0400

    att. media mpeg

--- src/routes/audio-route.ts
+++ src/routes/audio-route.ts
@@ -60,7 +60,7 @@ export async function audioRoutes(fastify: FastifyInstance) {
         const filePath = path.join(audioFolder, fileName);
         const buffer = await fs.readFile(filePath);
 
-        reply.header("Content-Type", "audio/ogg");
+        reply.header("Content-Type", "audio/mpeg");
         return reply.send(buffer);
       } catch (err) {
         console.error("ERRO AO BUSCAR O ÁUDIO:", err);
